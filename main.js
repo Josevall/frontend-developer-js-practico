8c81e6a8cbbd362338e722693bcf0ad22384c26c
Josevall <josevalles9314@gmail.com>
2022-08-04 06:04:01 -0400

    integrando el menu del shopoing-cart y haciendo que no se muestren solapados

--- main.js
+++ main.js
@@ -1,15 +1,40 @@
-const menuEmail = document.querySelector('.navbar-email')
-const desktopMenu = document.querySelector('.desktop-menu')
-const menuHamIcon = document.querySelector('.menu')
-const mobileMenu = document.querySelector('.mobile-menu')
+// Navbar-email desplegable
+
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
-menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
 }
+
+
+// mobile-menu desplegable
+
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
+}
+
+// navbar-shopping-cart desplegable
+
+const menuCartIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
+menuCartIcon.addEventListener('click', toggleCartAside);
+
+function toggleCartAside() {
+    aside.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive')
+
 }
